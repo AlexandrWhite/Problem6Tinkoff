19d23d0ad8a8729dd32320b9b55971f2e77b93be
Alexandr White <sashabelyy123@gmail.com>
2022-09-11 01:51:29 +0500

    добавил разбивку и тренировку

--- train.py
+++ train.py
@@ -1,18 +1,41 @@
 import argparse
-import numpy as np
+import sys
+import pickle
+import re
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--input-dir")
-#parser.add_argument("--model", required="True")
+# parser.add_argument("--model", required="True")
 
 args = parser.parse_args()
 
-data = []
+data_source = sys.stdin if args.input_dir is None else open(args.input_dir)
+model = {}
 
-if args.input_dir is None:
-    data = input().split()
-else:
-    f = open(args.input_dir)
-    line = f.readline().split()
 
+def create_ngram(data, n):
+    for line in data.readlines():
+        if not line.isspace():
+            line = line.lower()
+            line = re.sub(r'[^(\w|\s)]', '', line)
+            line = re.sub(r'\d|\(|\)|', '', line)
+            tokens = line.split()
+            for i in range(0, len(tokens) - n + 1):
+                yield ' '.join(tokens[i:i + n]), tokens[i + n] if i + n < len(tokens) else 'ENDWORD'
+
+
+def train_model():
+    for ngram, next_word in create_ngram(data_source, 2):
+        if ngram not in model.keys():
+            model[ngram] = {next_word: 1}
+        else:
+            if next_word not in model[ngram].keys():
+                model[ngram][next_word] = 1
+            else:
+                model[ngram][next_word] += 1
+
+
+train_model()
+for key, value in model.items():
+    print(key, value)
